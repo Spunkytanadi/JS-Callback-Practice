65e001da8c459edc1ab4ed9cf0035106ba997cb4
Marceilla Norman <101609514+Spunkytanadi@users.noreply.github.com>
2022-06-29 15:00:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,82 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
+    if(direction === 'west'){
+        character.src = `assets/green-character/west.gif`
+    }
+    if(direction === 'north'){
+        character.src = `assets/green-character/north.gif`
+    }
+    if(direction === 'east'){
+        character.src = `assets/green-character/east.gif`
+    }
+    if(direction === 'south'){
+        character.src = `assets/green-character/south.gif`
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+function moveWithArrowKeys(left, bottom){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+    
+    function moveCharacter(){ 
+        if(direction === 'west'){
+            x-=1
+        }
+        if(direction === 'north'){
+            y+=1
+        }
+        if(direction === 'east'){
+            x+=1
+        }
+        if(direction === 'south'){
+            y-=1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+    
+    setInterval(moveCharacter, 1)
+    
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+    
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        callback(direction)
+    })
+    
+    document.addEventListener('keyup', function(e){
+        direction = null
+        callback(direction)
+    })    
+}
+
+character.style.left = x + "px"
+character.style.bottom = y + "px"
 move(character).to(100, 250)
 
 
